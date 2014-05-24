9116e5ecfe9f0ea8cb25606d3d63b372116c0e46
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2014-05-24 11:18:44 +0300

    Update dependencies.

--- src/leiningen/new/cljs_webapp/project.clj
+++ src/leiningen/new/cljs_webapp/project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]]
-  :plugins [[lein-cljsbuild "1.0.2"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2202"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
 
   :cljsbuild {
     :builds {
